deb35dc66d9c3ab7b2ace68b9fb85c8b19897844
horoshi10v <valentyn.khoroshylov@gmail.com>
2022-06-08 14:52:12 +0300

    connect to mysql

--- api/main.go
+++ api/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+)
 
 func main() {
+
+	_, err := gorm.Open(mysql.Open("root:12345678@/showroomDB"), &gorm.Config{})
+	if err != nil {
+		log.Panicln("can't open database connection")
+	}
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
