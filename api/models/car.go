ff49cf71ee5276750e0729fe61140816bfacd547
horoshi10v <valentyn.khoroshylov@gmail.com>
2022-06-16 20:39:55 +0300

    add enum cat type

--- api/models/car.go
+++ api/models/car.go
@@ -1,24 +1,50 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"database/sql/driver"
+	"gorm.io/gorm"
+)
+
+type carType string
+
+const (
+	SEDAN     carType = "SEDAN"
+	HATCHBACK carType = "HATCHBACK"
+	MINIVAN   carType = "MINIVAN"
+)
+
+func (ct *carType) Scan(value interface{}) error {
+	*ct = carType(value.([]byte))
+	return nil
+}
+
+func (ct carType) Value() (driver.Value, error) {
+	return string(ct), nil
+}
 
 type Car struct {
 	gorm.Model
-	Id           uint   `json:"id" gorm:"primaryKey"`
-	Brand        string `json:"brand"`
-	Name         string `json:"name"`
-	Type         string `json:"type"`
-	Transmission string `json:"transmission"`
-	Color        string `json:"color"`
-	Images       string `json:"image"`
-	Description  string `json:"description"`
-	Engine       Engine `json:"engine" gorm:"embedded;embeddedPrefix:engine_"`
+	Id    uint   `json:"id" gorm:"primaryKey"`
+	Brand string `json:"brand"`
+	Name  string `json:"name"`
+	//Type         string `json:"type" gorm:"type:enum('published', 'pending', 'deleted');default:'pending'"`
+	Type         carType `sql:"type"`
+	Transmission string  `json:"transmission"`
+	Color        string  `json:"color"`
+	Images       string  `json:"image"`
+	Description  string  `json:"description"`
+	Engine       Engine  `json:"engine" gorm:"foreignKey:Id"`
 	VisitorList  VisitorList
 }
 
 type Engine struct {
-	Type            string `json:"engine_type"`
-	Size            uint   `json:"engine_size"`
-	Power           uint   `json:"engine_power"`
-	FuelConsumption uint   `json:"engine_fuel"`
+	Id              uint   `json:"id" gorm:"primaryKey"`
+	Type            string `json:"type"`
+	Size            uint   `json:"size"`
+	Power           uint   `json:"power"`
+	FuelConsumption uint   `json:"fuel"`
+}
+
+func (Car) TableName() string {
+	return "Cars"
 }
